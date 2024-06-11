c6bdf090c2a2a539ec4ab67efe7e5994112c1d7f
Gi-0n <74389815+Gi-0n@users.noreply.github.com>
2024-06-11 08:04:05 -0500

    Add files via upload

---
+++ service_worker_main.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const CACHE_NAME = 'cache-v1';
+// The files we want to cache
+const resourceList = [
+  '/',
+  'index.html',
+  'favicon.ico',
+  '/css/main.css',
+  '/css/clear-sans.css',
+  '/css/ClearSans-Bold-webfont.eot',
+  '/css/ClearSans-Bold-webfont.svg',
+  '/css/ClearSans-Bold-webfont.woff',
+  '/css/ClearSans-Light-webfont.eot',
+  '/css/ClearSans-Light-webfont.svg',
+  '/css/ClearSans-Light-webfont.woff',
+  '/css/ClearSans-Regular-webfont.eot',
+  '/css/ClearSans-Regular-webfont.svg',
+  '/css/ClearSans-Regular-webfont.woff',
+  '/js/animframe_polyfill.js',
+  '/js/application.js',
+  '/js/bind_polyfill.js',
+  '/js/classlist_polyfill.js',
+  '/js/game_manager.js',
+  '/js/grid.js',
+  '/js/actuator.js',
+  '/js/keyboard_input_manager.js',
+  '/js/local_storage_manager.js',
+  '/js/tile.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+    return cache.addAll(resourceList);
+  }));
+});
+
+function addToCache(cacheName, resourceList) {
+  caches.open(cacheName).then(cache => {
+    return cache.addAll(resourceList);
+  });
+}
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    return response || fetch(event.request);
+  }));
+});
